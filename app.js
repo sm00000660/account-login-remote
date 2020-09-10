f5fecea61039bbc9f4bab2c13a389ade5d1e30ba
Jay Chen <sm00000660@gmail.com>
2020-09-10 17:35:51 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const port = 3000;
+
+mongoose.connect("mongodb://localhost/users-list", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", () => {
+  console.log("mongodb error");
+});
+
+db.once("open", () => {
+  console.log("mongoose connected");
+});
 
 app.get("/", (req, res) => {
   res.send("hello word");
